037bfbe84e1c49b793ffc1045157817b9b8b3ad0
buchananwill <139961977+buchananwill@users.noreply.github.com>
2025-05-12 16:54:59 +0100

    Adds visitor base class

---
+++ Components/tag_node_tree_visitor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by thele on 12/05/2025.
+//
+
+#include "tag_node_tree_visitor.h"
---
+++ Components/tag_node_tree_visitor.h
@@ -0,0 +1,16 @@
+//
+// Created by thele on 12/05/2025.
+//
+
+#ifndef TAG_NODE_TREE_VISITOR_H
+#define TAG_NODE_TREE_VISITOR_H
+
+
+
+class tag_node_tree_visitor {
+
+};
+
+
+
+#endif //TAG_NODE_TREE_VISITOR_H
