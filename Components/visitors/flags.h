c3e1c97e0f58b60a735c3022a6e55bfa1831ba54
buchananwill <139961977+buchananwill@users.noreply.github.com>
2025-07-08 13:02:31 +0100

    Separates cpp from h file

---
+++ Components/visitors/flags.h
@@ -0,0 +1,12 @@
+//
+// Created by thele on 08/07/2025.
+//
+
+#ifndef FLAGS_H
+#define FLAGS_H
+
+namespace visitor::flags {
+	constexpr auto supplier_attribute = "SupplierAttribute";
+}
+
+#endif //FLAGS_H
--- Components/visitors/gameplay_tag_supplier_processor.cpp
+++ Components/visitors/gameplay_tag_supplier_processor.cpp
@@ -9,7 +9,13 @@
 #include <fstream>
 #include <iostream>
 
-inline constexpr std::format_string<const std::string&> processor_template{R"(
+#include "flags.h"
+
+inline constexpr std::format_string<const std::string&> header_template{R"(
+/*
+* This file is auto-generated and should not be edited.
+*/
+
 #pragma once
 #include "SupplierUtilityMixin.h"
 #include "CoreMinimal.h"
@@ -43,43 +49,52 @@ public:
 	}}
 
 
-	virtual void ConfigureQueries() override
-	{{
-		Resort::Mass::Economy::ConfigureQueries<F{0}Attribute, F{0}Archetypes>(EntityQuery);
-	}}
+	virtual void ConfigureQueries() override;
 
-	virtual void Execute(FMassEntityManager& EntityManager, FMassExecutionContext& Context) override
-	{{
-		Resort::Mass::Economy::Execute<F{0}Attribute, F{0}Archetypes>(EntityManager, Context, EntityQuery);
-	}}
+	virtual void Execute(FMassEntityManager& EntityManager, FMassExecutionContext& Context) override;
 
 private:
 	FMassEntityQuery EntityQuery;
 }};
 )"};
 
-constexpr auto supplier_attribute_flag = "SupplierAttribute";
+inline constexpr std::format_string<const std::string&> cpp_template{R"(
+#include "{0}Processor.h"
+
+void U{0}UtilityProcessor::ConfigureQueries()
+{{
+	Resort::Mass::Economy::ConfigureQueries<F{0}Attribute, F{0}Archetypes>(EntityQuery);
+}}
+
+void U{0}UtilityProcessor::Execute(FMassEntityManager& EntityManager, FMassExecutionContext& Context)
+{{
+	Resort::Mass::Economy::Execute<F{0}Attribute, F{0}Archetypes>(EntityManager, Context, EntityQuery);
+}}
+
+)"};
 
 bool gameplay_tag_supplier_processor::visit_tree(const std::shared_ptr<TagNode> &root) {
 	return tag_node_tree_visitor::visit_tree(root);
 }
 
 void gameplay_tag_supplier_processor::process_node(const TagNode &node) {
-	if (!std::ranges::any_of(node.Flags, [](const std::string &flag) { return flag == supplier_attribute_flag; })) {
+	if (!std::ranges::any_of(node.Flags, [](const std::string &flag) { return flag == visitor::flags::supplier_attribute; })) {
 		return;
 	}
 
-	std::ofstream header_file;
-	header_file.open("test.h");
+	std::ofstream header_file(std::format(R"({}Processor.h)", node.Name));
+	std::ofstream cpp_file(std::format(R"({}Processor.cpp)", node.Name));
 
-	if (!header_file.is_open()) {
+	if (!header_file.is_open() || !cpp_file.is_open()) {
 		std::cout << "Couldn't open file\n";
 		return;
 	}
 
-	auto header_file_content = std::format(processor_template, node.Name);
-
+	auto header_file_content = std::format(header_template, node.Name);
 	header_file << header_file_content;
-
 	header_file.close();
+
+	auto cpp_file_content = std::format(cpp_template, node.Name);
+	cpp_file << cpp_file_content;
+	cpp_file.close();
 }
